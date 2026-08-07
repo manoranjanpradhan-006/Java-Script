ca1723249878ea7eb9454d7ac5a8a3bf67159909
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-07 18:56:14 +0530

    Add string and number examples, including concatenation and random number generation

---
+++ 01-Basics/05_string.js
@@ -0,0 +1,50 @@
+const nickName = "chinu";
+const goodName = new String("Man-oran-jan");
+
+// console.log(nickName);
+// console.log(goodName);
+// console.log(goodName[0]);
+// console.log(goodName[1]);
+// console.log(goodName[2]);
+// console.log(goodName[3]);
+// console.log(goodName[4]);
+// console.log(goodName[5]);
+// console.log(goodName[6]);
+// console.log(goodName[7]);
+// console.log(goodName[8]);
+// console.log(goodName[9]);
+
+// console.log(goodName.__proto__);
+// console.log(goodName.length);
+// console.log(goodName.toUpperCase());
+// console.log(goodName.charAt(7));
+// console.log(goodName.indexOf("a"));
+
+const newString = goodName.substring(0, 5);
+// console.log(newString);
+
+const slicedString = goodName.slice(-3, 3);
+// console.log(slicedString);
+
+const newName = "   chinu";
+// console.log(newName);
+// console.log(newName.trim());
+
+const url = "https://youtube.com/manoranjan%20pradhan";
+
+// console.log(url);
+// console.log(url.replace("%20", "-"));
+// console.log(url.includes("manoranjan"));
+
+console.log(goodName.split("-"));
+
+//Basic concatenation
+
+// const name = "Chinu";
+// const repoCount = 15;
+
+// console.log(name + repoCount + " ..");
+
+//Using String Interpolation
+
+// console.log(`hey, My name is ${name} and my curr repo count is ${repoCount}`);
---
+++ 01-Basics/06_nums.js
@@ -0,0 +1,26 @@
+const score = 400;
+
+const balance = new Number(100);
+
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+const otherNumber = 122.984;
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000000;
+
+// console.log(hundreds.toLocaleString("en-In"));
+
+// console.log(Math.abs(-954849));
+// console.log(Math.round(95.4903));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10 + 1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
