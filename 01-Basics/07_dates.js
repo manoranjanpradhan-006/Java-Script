974991a17adf847c676ed6acb996a6cf024a5c03
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-07 19:05:45 +0530

    Refactor date logging to display full date components and add a specific date example

--- 01-Basics/07_dates.js
+++ 01-Basics/07_dates.js
@@ -1,1 +1,5 @@
-console.log(new Date().getFullYear());
+const date = new Date();
+
+console.log(date.getFullYear(), date.getMonth(), date.getDate());
+
+console.log(new Date(2026, 7, 7, 19, 3).toString());
