4244c3fde0609601812d864f8cd18ce8cae6071a
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-08 21:05:21 +0530

    Enable date logging by uncommenting relevant console statements and add weekday formatting

--- 01-Basics/07_dates.js
+++ 01-Basics/07_dates.js
@@ -1,12 +1,15 @@
 const date = new Date();
 
-// console.log(date.getFullYear(), date.getMonth(), date.getDate());
+console.log(date.getFullYear(), date.getMonth(), date.getDate());
 
 // console.log(new Date(2026, 7, 7, 19, 3).toString());
-
 // console.log(Math.floor(Date.now() / 1000));
 
-console.log(date);
-console.log(date.getFullYear());
-console.log(date.getMonth());
-console.log(date.getDay());
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDay());
+
+date.toLocaleString("default", {
+  weekday: "long",
+});
