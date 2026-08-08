eb45b9e13915da46a92dabbfa3326d2123037b0a
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-08 20:52:41 +0530

    .

--- 01-Basics/07_dates.js
+++ 01-Basics/07_dates.js
@@ -3,3 +3,5 @@ const date = new Date();
 console.log(date.getFullYear(), date.getMonth(), date.getDate());
 
 console.log(new Date(2026, 7, 7, 19, 3).toString());
+
+console.log(Math.floor(Date.now() / 1000));
