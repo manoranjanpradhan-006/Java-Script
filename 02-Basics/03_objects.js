c4630d6e0a958d8e4d7b7eace47d0eec41142f07
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-10 16:37:11 +0530

    Add initial object literal and logging examples for jsUser

---
+++ 02-Basics/03_objects.js
@@ -0,0 +1,21 @@
+// Singleton
+
+// Object literals
+
+const jsUser = {
+  name: "chinu",
+  fullName: "Manoranjan",
+  age: "18",
+  email: "@gmail.com",
+  isLoggedIn: "false",
+};
+
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+console.log(jsUser["fullName"]);
+
+console.log("hii");
+
+for (let i = 0; i < 8; i++) {
+  console.log("chinu");
+}
