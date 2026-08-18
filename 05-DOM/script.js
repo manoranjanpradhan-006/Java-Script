bff88ad498f17f955e8c60a41bb21f151f49ed75
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-18 13:24:22 +0530

    Refactor HTML structure and enhance JavaScript event handling for tea list

--- 05-DOM/script.js
+++ 05-DOM/script.js
@@ -5,3 +5,9 @@ console.log(document);
 document.getElementById("click").addEventListener("click", function () {
   alert("there nothing to show");
 });
+
+document.querySelector(".tealist").addEventListener("click", function (event) {
+  if (event.target) {
+    alert(event.target.textContent);
+  }
+});
